f5ac37af5c717f713db74958db6ac61220f04da4
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2024-01-24 14:11:29 +0300

    add getUsersInfo tests

--- src/test/java/UsersTests.java
+++ src/test/java/UsersTests.java
@@ -4,10 +4,31 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.is;
 
 public class UsersTests {
+    @Test
+    @Feature("UsersProfile")
+    @Story("Get user info")
+    @Owner("oyaritskayao")
+    void getUsersList() {
+
+        String firstName = "Lindsay";
+        String lastName = "Ferguson";
+        int usersCount = 12;
+
+        given()
+                .log().uri()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().body()
+                .log().status()
+                .body("data[1].first_name", equalToIgnoringCase(firstName))
+                .body("data[1].last_name", equalToIgnoringCase(lastName))
+                .body("total", is(usersCount))
+                .statusCode(200);    }
+
     @Test
     @Feature("UsersProfile")
     @Story("Get user info")
@@ -19,8 +40,21 @@ public class UsersTests {
                 .then()
                 .log().body()
                 .log().status()
-                .body("data.first_name", hasKey("Janet"))
-                .body("data.last_name", hasKey("Weaver"))
+                .body("data.first_name", equalToIgnoringCase("Janet"))
+                .body("data.last_name", equalToIgnoringCase("Weaver"))
                 .body("data.id", is(2));
+    }
+    @Test
+    @Feature("UsersProfile")
+    @Story("Get user info")
+    @Owner("oyaritskayao")
+    void singleUserNotFound() {
+        given().
+                log().uri()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(404);
     }
 }
